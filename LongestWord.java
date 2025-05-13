c41201ef6e2bcfc32923b5a391843e81f4e9d8cc
ankitajagtap04@gmail.com <ankitajagtap04@gmail.com>
2025-05-13 16:58:40 +0530

    Find the longest word using sort

--- LongestWord.java
+++ LongestWord.java
@@ -1,10 +1,18 @@
+import java.util.*;
+import java.lang.*;
+
 public class LongestWord{
     
     public static void main(String args[]){
-        String text = "Ankita Mukund Jagtap";
+        String text = "Ready, steady, go!";
         String[] str = text.split(" ");
         int maxLength = Integer.MIN_VALUE;
         String ans = "";
+        Arrays.sort(str, Collections.reverseOrder());
+        int n1 = str.length;
+
+        System.out.println(str[0]);
+
         for(String name: str){
             int n = name.length();
             if(n > maxLength){
